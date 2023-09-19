4a88b499683b9f60282045d07561b14e8fbbd5ef
Bagrat Arutyunov <arutyunovbagrat@gmail.com>
2023-09-19 14:09:24 +0300

    Initial Commit

---
+++ WildAvia/WildAviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  WildAviaApp.swift
+//  WildAvia
+//
+//  Created by Bagrat Arutyunov on 19.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct WildAviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
